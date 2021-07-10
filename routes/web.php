d1c2929d908737c044a216d23de030b12e9557ae
fiqih16 <fiqih1666@gmail.com>
2021-07-10 18:35:18 +0700

    Hapus Method Show User

--- routes/web.php
+++ routes/web.php
@@ -58,5 +58,5 @@ Route::group(['prefix' => 'dashboard','middleware' => ['web','auth']],function()
     Route::get('/roles/select', [\App\Http\Controllers\RoleController::class,'select'])->name('roles.select');
     Route::resource('/roles', \App\Http\Controllers\RoleController::class);
     // UserController
-    Route::resource('/users', \App\Http\Controllers\UserController::class);
+    Route::resource('/users', \App\Http\Controllers\UserController::class)->except(['show']);
 });
