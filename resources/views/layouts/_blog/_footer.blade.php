1dca471406294313dbb042998f5753ca1433c110
fiqih16 <fiqih1666@gmail.com>
2021-07-17 21:25:17 +0700

    Perbarui Tampilan Footer

--- resources/views/layouts/_blog/_footer.blade.php
+++ resources/views/layouts/_blog/_footer.blade.php
@@ -1,5 +1,98 @@
-<footer class="py-5 footer-color mt-auto">
+{{-- <footer class="py-5 footer-color mt-auto">
     <div class="container">
        <p class="m-0 text-center text-white">Copyright &copy; {{ config('app.name')}} 2021</p>
     </div>
- </footer>
+ </footer> --}}
+
+
+  <!-- Footer -->
+  <footer class="mt-auto text-center text-white" style="background-color: #004d66">
+    <!-- Grid container -->
+    <div class="container">
+      <!-- Section: Links -->
+      <section class="mt-3">
+        <!-- Grid row-->
+        <div class="row text-center d-flex justify-content-center pt-2">
+          <!-- Grid column -->
+          <div class="col-md-2">
+            <h6 class="text-uppercase font-weight-bold">
+              <a href="#!" class="text-white">About</a>
+            </h6>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2">
+            <h6 class="text-uppercase font-weight-bold">
+              <a href="#!" class="text-white">Privacy Policy</a>
+            </h6>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2">
+            <h6 class="text-uppercase font-weight-bold">
+              <a href="#!" class="text-white">Disclaimer</a>
+            </h6>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2">
+            <h6 class="text-uppercase font-weight-bold">
+              <a href="#!" class="text-white">Sitemap</a>
+            </h6>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2">
+            <h6 class="text-uppercase font-weight-bold">
+              <a href="#!" class="text-white">Contact</a>
+            </h6>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row-->
+      </section>
+      <!-- Section: Links -->
+
+      <hr class="my-3" />
+
+      <!-- Section: Text -->
+      <section class="mb-3">
+        <div class="row d-flex justify-content-center">
+          <div class="col-lg-8">
+            <p>
+              "Mimpi Besar Berawal Dari Satu Langkah Kecil."
+            </p>
+          </div>
+        </div>
+      </section>
+      <!-- Section: Text -->
+
+      <!-- Section: Social -->
+      <section class="text-center mb-3">
+        <a href="" class="text-white me-4 p-2">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="" class="text-white me-4 p-2">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a href="" class="text-white me-4 p-2">
+          <i class="fab fa-github"></i>
+        </a>
+      </section>
+      <!-- Section: Social -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div
+         class="text-center p-2"
+         style="background-color: rgba(0, 0, 0, 0.2)">
+      <p class="pt-2 text-white">© 2021 Copyright {{ config('app.name')}}</p>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
